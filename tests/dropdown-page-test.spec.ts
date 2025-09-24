8315a62b759dc125e1b319ad726723bb377b90aa
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-23 20:30:04 -0500

    added 2 test

--- tests/dropdown-page-test.spec.ts
+++ tests/dropdown-page-test.spec.ts
@@ -1,11 +1,20 @@
 import { test } from "@playwright/test";
 import { HomePage } from "../pages/home-page";
 import { DropdownPage } from "../pages/dropdown-page";
-test("Dropdown validation", async ({ page }) => {
+test("Dropdown validation Option 1 ", async ({ page }) => {
   let dropdownPage = new DropdownPage(page);
   let homePage = new HomePage(page);
   await page.goto("https://the-internet.herokuapp.com/");
-  await homePage.navigateToDropdown();
-  await dropdownPage.selectOption("Option 1");
-  await dropdownPage.validationofOptionList("1");
+  await homePage.clickOnLink('Dropdown')
+  await dropdownPage.selectOption('1');
+    await dropdownPage.validateSelectedOption('1')
 });
+
+test("Dropdown validation Option 2 ", async ({ page }) => {
+    let dropdownPage = new DropdownPage(page);
+    let homePage = new HomePage(page);
+    await page.goto("https://the-internet.herokuapp.com/");
+    await homePage.clickOnLink('Dropdown')
+    await dropdownPage.selectOption('2');
+    await dropdownPage.validateSelectedOption('2')
+})

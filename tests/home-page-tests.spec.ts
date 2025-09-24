d6cef51975199c2ae8761c3dfaeb40f92cb27dd4
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-23 19:18:31 -0500

    no spaces

--- tests/home-page-tests.spec.ts
+++ tests/home-page-tests.spec.ts
@@ -1,6 +1,5 @@
 import { test } from "@playwright/test";
 import { HomePage } from "../pages/home-page";
-
 test("Validation of home page title", async ({ page }) => {
   let homePage = new HomePage(page);
   await page.goto("https://the-internet.herokuapp.com");

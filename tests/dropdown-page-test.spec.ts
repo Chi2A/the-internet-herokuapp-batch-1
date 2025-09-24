311edb8166ed3fd95074f02a86a743098319028d
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-23 19:19:20 -0500

    added homepage

--- tests/dropdown-page-test.spec.ts
+++ tests/dropdown-page-test.spec.ts
@@ -1,11 +1,11 @@
 import { test } from "@playwright/test";
-import { DropdownPage } from "../pages/dropdown-page"
-
-test('Dropdown validation', async ({ page }) => { 
-    let dropdownPage = new DropdownPage(page);
-    await page.goto("https://the-internet.herokuapp.com/");
-    await homePage.navigateToDropdown()
-    await dropdownPage.selectOption('Option 1')
-
-    await dropdownPage.validationofOptionList('1')
-})
+import { HomePage } from "../pages/home-page";
+import { DropdownPage } from "../pages/dropdown-page";
+test("Dropdown validation", async ({ page }) => {
+  let dropdownPage = new DropdownPage(page);
+  let homePage = new HomePage(page);
+  await page.goto("https://the-internet.herokuapp.com/");
+  await homePage.navigateToDropdown();
+  await dropdownPage.selectOption("Option 1");
+  await dropdownPage.validationofOptionList("1");
+});

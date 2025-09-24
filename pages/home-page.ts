e891f56b9e17422aa2452b425ebc96ea99a19e4c
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-23 19:08:27 -0500

    added locator

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,10 +1,15 @@
 import { expect, Page, Locator } from "@playwright/test";
 export class HomePage {
-  mainTitleLocator: Locator;
+    mainTitleLocator: Locator;
+    dropdownLocator: Locator;
   constructor(page: Page) {
     this.mainTitleLocator = page.locator('h1[class="heading"]');
+    this.dropdownLocator = page.locator('a[href="/dropdown"]');
   }
   async validateMainTitle(expectedTitle: string): Promise<void> {
     await expect(this.mainTitleLocator).toHaveText(expectedTitle);
+  }
+  async navigateToDropdown(): Promise<void> {
+    await this.dropdownLocator.click();
   }
 }

8a9282e662f1dcacd9e879c1d9dbdd145d7b81fd
Chi2A <154117631+Chi2A@users.noreply.github.com>
2025-09-25 19:01:20 -0500

    formatted

--- pages/checkbox-page.ts
+++ pages/checkbox-page.ts
@@ -1,22 +1,19 @@
-import { Page,Locator,expect} from "@playwright/test";
+import { Page, Locator, expect } from "@playwright/test";
 import { HomePage } from "./home-page";
 export class CheckboxPage {
-    checkbox1Locator: Locator;
-    checkbox2Locator: Locator;
-    constructor(page: Page) {
-        this.checkbox1Locator = page.getByRole('checkbox').first();
-        this.checkbox2Locator = page.getByRole('checkbox').nth(1);
-
-    }
-    async validateCheckbox1IsNotChecked(): Promise<void> { 
-        await expect(this.checkbox1Locator).not.toBeChecked();
-    }
-    async validateCheckbox2IsNotChecked(): Promise<void> { 
-        await expect(this.checkbox2Locator).toBeChecked();
-    
-    }
-    async validateCheckBox1IsChecked(): Promise<void> { 
-        await expect(this.checkbox1Locator).toBeChecked();
-        
-    }
+  checkbox1Locator: Locator;
+  checkbox2Locator: Locator;
+  constructor(page: Page) {
+    this.checkbox1Locator = page.getByRole("checkbox").first();
+    this.checkbox2Locator = page.getByRole("checkbox").nth(1);
+  }
+  async validateCheckbox1IsNotChecked(): Promise<void> {
+    await expect(this.checkbox1Locator).not.toBeChecked();
+  }
+  async validateCheckbox2IsNotChecked(): Promise<void> {
+    await expect(this.checkbox2Locator).toBeChecked();
+  }
+  async validateCheckBox1IsChecked(): Promise<void> {
+    await expect(this.checkbox1Locator).toBeChecked();
+  }
 }
--- pages/home-page.ts
+++ pages/home-page.ts
@@ -1,13 +1,13 @@
-import { Page,Locator,expect } from "@playwright/test";
-export class HomePage { 
-    checkBoxLinkLocator: Locator;
+import { Page, Locator, expect } from "@playwright/test";
+export class HomePage {
+  checkBoxLinkLocator: Locator;
 
-    constructor(page: Page) { 
-        this.checkBoxLinkLocator = page.getByRole("link", {
-          name: "Checkboxes",
-        });
-    }
-    async navigateToCheckboxLink(): Promise<void> { 
-        await this.checkBoxLinkLocator.click();
-    }
+  constructor(page: Page) {
+    this.checkBoxLinkLocator = page.getByRole("link", {
+      name: "Checkboxes",
+    });
+  }
+  async navigateToCheckboxLink(): Promise<void> {
+    await this.checkBoxLinkLocator.click();
+  }
 }
